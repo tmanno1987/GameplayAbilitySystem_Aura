7b1aeb064e3a9aa234b6c0870ff9ac476863298f
Timothy Manno <tmanno1987@gmail.com>
2024-02-16 08:47:22 -0800

    Generated EnemyInterface and added to AuraEnemy base class.

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -5,13 +5,17 @@
 #include "CoreMinimal.h"
 #include "Character/CharacterBase.h"
 #include "AuraEnemy.generated.h"
+#include "Interaction/EnemyInterface.h"
 
 /**
  * 
  */
 UCLASS()
-class AURA_API AAuraEnemy : public ACharacterBase
+class AURA_API AAuraEnemy : public ACharacterBase, public IEnemyInterface
 {
 	GENERATED_BODY()
 	
+public:
+	virtual void HighlightActor() override;
+	virtual void UnHighlightActor() override;
 };
