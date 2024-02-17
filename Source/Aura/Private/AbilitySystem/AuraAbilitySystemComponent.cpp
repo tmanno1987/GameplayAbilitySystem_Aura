e1977cace808f1cd192be71ca8336b24e3c16eb3
Timothy Manno <tmanno1987@gmail.com>
2024-02-17 01:13:35 -0800

    Setup AbilitySystemInterface to PlayerState and AuraEnemy

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -0,0 +1,5 @@
+// Copyright Timothy Manno
+
+
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+
--- Source/Aura/Public/Character/CharacterBase.h
+++ Source/Aura/Public/Character/CharacterBase.h
@@ -4,20 +4,33 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "AbilitySystemInterface.h"
+
 #include "CharacterBase.generated.h"
+
+class UAbilitySystemComponent;
+class UAttributeSet;
 
 UCLASS(Abstract)
 
-class AURA_API ACharacterBase : public ACharacter
+class AURA_API ACharacterBase : public ACharacter, public IAbilitySystemInterface
 {
 	GENERATED_BODY()
 
 public:
 	ACharacterBase();
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+	UAttributeSet* GetAttributeSet() const { return AttributeSet; }
 
 protected:
 	virtual void BeginPlay() override;
 
 	UPROPERTY(EditAnywhere, Category = "Combat")
 	TObjectPtr<USkeletalMeshComponent> Weapon;
+
+	UPROPERTY()
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+
+	UPROPERTY()
+	TObjectPtr<UAttributeSet> AttributeSet;
 };
