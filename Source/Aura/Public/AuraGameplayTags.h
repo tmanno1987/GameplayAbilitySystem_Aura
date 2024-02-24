450f71b4e67c6f1ba9be6f43e623e271c4fbd5f1
Timothy Manno <tmanno1987@gmail.com>
2024-02-24 02:57:18 -0800

    Created a singleton for the purpose of getting gameplay tags

---
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -0,0 +1,25 @@
+// Copyright Timothy Manno
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+
+/**
+ *  AuraGameplayTags
+ * 
+ *  Singleton containing native GameplayTags
+ */
+
+struct FAuraGameplayTags
+{
+public:
+	static const FAuraGameplayTags& Get() { return GameplayTags; }
+	static void InitializeNativeGameplayTags();
+
+protected:
+
+
+private:
+	static FAuraGameplayTags GameplayTags;
+};
